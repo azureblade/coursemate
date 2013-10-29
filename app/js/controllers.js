3cbfa6a9a9daf2876177caac649fe42fc6098035
Marshall Bowers <Marshall@Marshalls-MacBook-Air.local>
2013-10-29 10:23:54 -0400

    Added DB spoofing via JSON.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,26 +6,38 @@ function CoursesCtrl($scope) {
 
 }
 
-function FriendListCtrl($scope) {
-	$scope.friends = [
-		{
-			Username: "wrecking_ball",
-			Name: "Miley Cyrus",
-			School: "Twerk U"
-		},
-		{
-			Username: "holy_angel",
-			Name: "Misha Collins",
-			School: "Divine Academy"
-		},
-		{
-			Username: "bbatch",
-			Name: "Benedict Cumberbatch",
-			School: "Starfleet"
-		}
-	];
+function FriendListCtrl($scope, $http) {
+	$http.get('./friends.json').success(function(data) {
+		$scope.friends = data;
+	});
+	// $scope.friends = [
+	// 	{
+	// 		Username: "wrecking_ball",
+	// 		Name: "Miley Cyrus",
+	// 		School: "Twerk U"
+	// 	},
+	// 	{
+	// 		Username: "holy_angel",
+	// 		Name: "Misha Collins",
+	// 		School: "Divine Academy"
+	// 	},
+	// 	{
+	// 		Username: "bbatch",
+	// 		Name: "Benedict Cumberbatch",
+	// 		School: "Starfleet"
+	// 	}
+	// ];
 }
 
-function FriendProfileCtrl($scope, $routeParams) {
+function FriendProfileCtrl($scope, $routeParams, $http) {
 	$scope.Username = $routeParams.Username;
+
+	$http.get('./friends.json').success(function(data) {
+		for (var i = 0; i < data.length; i++) {
+			if (data[i].Username === $routeParams.Username) {
+				$scope.Friend = data[i];
+				return;
+			}
+		}
+	});
 }
