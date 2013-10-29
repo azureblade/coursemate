600ced90df333ab8f86a8a3afff7749daa0531d7
Marshall Bowers <Marshall@Marshalls-MacBook-Air.local>
2013-10-29 14:51:30 -0400

    Updated Calendar view
    Adding code to generate the current month based on current date.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -24,7 +24,84 @@ function NavigationCtrl($scope) {
 }
 
 function CalendarCtrl($scope) {
+	function generate() {
+		var today = new Date();
 
+		var month = [];
+
+		for (var i = 1; i < daysInMonth(today.getMonth()); i++) {
+			
+		}
+	}
+
+	function daysInMonth(month) {
+		var today = new Date();
+		var days;
+
+		switch(month + 1) {
+			case 4:
+			case 6:
+			case 9:
+			case 11:
+				days = 30;
+				break;
+			case 2:
+				days = (today.getFullYear() % 4 === 0) ? 29 : 28;
+				break;
+			default:
+				days = 31;
+		}
+
+		return days
+	}
+
+	$scope.month = [
+		{
+			"Sunday": "", 
+			"Monday": "",
+			"Tuesday": "1",
+			"Wednesday": "2",
+			"Thursday": "3",
+			"Friday": "4",
+			"Saturday": "6"
+		},
+		{
+			"Sunday": "7", 
+			"Monday": "8",
+			"Tuesday": "9",
+			"Wednesday": "10",
+			"Thursday": "11",
+			"Friday": "12",
+			"Saturday": "13"
+		},
+		{
+			"Sunday": "14", 
+			"Monday": "15",
+			"Tuesday": "16",
+			"Wednesday": "17",
+			"Thursday": "18",
+			"Friday": "19",
+			"Saturday": "20"
+		},
+		{
+			"Sunday": "21", 
+			"Monday": "22",
+			"Tuesday": "23",
+			"Wednesday": "24",
+			"Thursday": "25",
+			"Friday": "26",
+			"Saturday": "27"
+		},
+		{
+			"Sunday": "28", 
+			"Monday": "29",
+			"Tuesday": "30",
+			"Wednesday": "31",
+			"Thursday": "",
+			"Friday": "",
+			"Saturday": ""
+		}
+	];
 }
 
 function CourseListCtrl($scope, $http) {
